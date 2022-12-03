2584400bec49ef68e51625c56721dc99d809d65d
Hritvik Mohan <hritvik5005@gmail.com>
2022-12-03 10:41:22 +0530

    Add Forgot Password and Update Profile

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,26 +1,27 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function ForgotPassword() {
 
     const emailRef = useRef()
-    const { login } = useAuth()
+    const { resetPassword } = useAuth()
     const [error, setError] = useState('')
+    const [message, setMessage] = useState('')
     const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
     
     async function handleSubmit(e) {
         e.preventDefault()
 
         try {
+            setMessage('')
             setError('')
             setLoading(true)
-            // await login(emailRef.current.value, passwordRef.current.value)
-            navigate('/')
+            await resetPassword(emailRef.current.value)
+            setMessage('Check your inbox for further instructions')
         } catch {
-            setError('Failed to log in')
+            setError('Failed to reset password')
         }
         setLoading(false)
     }
@@ -33,6 +34,7 @@ export default function ForgotPassword() {
                         Password Reset
                     </h2>
                     {error && <Alert variant="danger">{error}</Alert>}
+                    {message && <Alert variant="success">{message}</Alert>}
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
